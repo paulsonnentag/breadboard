1c517372e87ee79e1ea82e95134417d64db49b37
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-02-13 15:23:48 +0100

    Add location picker

--- src/widgets/MapWidget.tsx
+++ src/widgets/MapWidget.tsx
@@ -1,6 +1,5 @@
 import { getWidgetsOnMap, Widget } from "./index"
 import { useEffect, useId, useMemo, useRef, useState } from "react"
-import { LocationOverride } from "./LocationWidget"
 import { debounce } from "lodash"
 import LatLngLiteral = google.maps.LatLngLiteral
 import Map = google.maps.Map
@@ -9,7 +8,9 @@ import AdvancedMarkerView = google.maps.marker.AdvancedMarkerView
 import LatLngBounds = google.maps.LatLngBounds
 import GeocoderResult = google.maps.GeocoderResult
 import MapsEventListener = google.maps.MapsEventListener
-import { Cross2Icon } from "@radix-ui/react-icons"
+import { ArrowUpIcon, Cross2Icon, ResetIcon } from "@radix-ui/react-icons"
+import { useOnClickOutside } from "../hooks"
+import AutocompletePrediction = google.maps.places.AutocompletePrediction
 
 export interface MapLocation {
   name: string
@@ -36,7 +37,6 @@ export function MapWidgetView({ widget, onChange, widgetsInScope, onDestroy }: M
   const listenersRef = useRef<MapsEventListener[]>([])
   const markersRef = useRef<AdvancedMarkerView[]>([])
   const containerRef = useRef<HTMLDivElement>(null)
-  const [currentOverride, setCurrentOverride] = useState<LocationOverride>()
   const geoCoder = useMemo(() => new google.maps.Geocoder(), [])
   const widgetsOnMap = getWidgetsOnMap(widgetsInScope)
 
@@ -162,17 +162,27 @@ export function MapWidgetView({ widget, onChange, widgetsInScope, onDestroy }: M
   useEffect(() => {
     if (
       mapRef.current &&
-      !currentOverride &&
       !mapRef.current?.getCenter()?.equals(new LatLng(widget.location.latLng))
     ) {
       mapRef.current.setCenter(widget.location.latLng)
     }
-  }, [widget.location.latLng, currentOverride])
+  }, [widget.location.latLng])
 
   return (
-    <div className="flex flex-col w-full h-full bg-white rounded-xl shadow overflow-hidden">
-      <div className="flex p-2 items-center justify-between">
+    <div className="flex flex-col w-full h-full bg-white rounded-xl shadow">
+      <div className="flex p-2 items-center gap-2">
         <div className="text-green-600 p-2">Map</div>
+
+        <div className="flex-1"></div>
+
+        <LocationPickerView
+          value={widget.location}
+          onChange={(newLocation) => {
+            onChange((widget) => {
+              widget.location = newLocation
+            })
+          }}
+        />
 
         <button onClick={onDestroy}>
           <Cross2Icon />
@@ -210,4 +220,116 @@ function getBiggestContainedResult(
   }
 
   return results[0]
+}
+
+interface LocationPickerViewProps {
+  value: MapLocation
+  onChange: (location: MapLocation) => void
+}
+
+export function LocationPickerView({ value, onChange }: LocationPickerViewProps) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  useOnClickOutside(ref, () => setIsOpen(false))
+
+  return (
+    <div className="relative">
+      <div className="bg-gray-200 rounded-xl px-1 text-purple-700 flex text-xs items-center">
+        <div
+          className="p-2"
+          onClick={(evt) => {
+            setIsOpen((isOpen) => !isOpen)
+            evt.stopPropagation()
+          }}
+        >
+          {value.name}
+        </div>
+      </div>
+
+      {isOpen && (
+        <div
+          ref={ref}
+          className="absolute -top-3 left-full w-[300px] h-[300px] bg-white z-50 rounded-xl shadow ml-3"
+        >
+          <LocationSearch value={value} onChange={onChange} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+interface LocationSearchProps {
+  value: MapLocation
+  onChange: (widget: MapLocation) => void
+}
+
+export function LocationSearch({ value, onChange }: LocationSearchProps) {
+  const [search, setSearch] = useState("")
+  const [predictions, setPredictions] = useState<AutocompletePrediction[]>([])
+  const autocomplete = useMemo(() => new google.maps.places.AutocompleteService(), [])
+  const placesService = useMemo(
+    () => new google.maps.places.PlacesService(document.createElement("div")),
+    []
+  )
+
+  useEffect(() => {
+    if (search === "") {
+      setPredictions([])
+      return
+    }
+
+    autocomplete
+      .getPlacePredictions({
+        input: search,
+      })
+      .then((result) => {
+        setPredictions(result.predictions)
+      })
+  }, [search])
+
+  const onSelectPlace = (id: string) => {
+    setSearch("")
+    setPredictions([])
+
+    placesService.getDetails({ placeId: id }, (placeResult) => {
+      if (placeResult) {
+        const { name, geometry } = placeResult
+        if (name && geometry && geometry.location) {
+          onChange({ name, latLng: geometry.location.toJSON() })
+        }
+      }
+    })
+  }
+
+  return (
+    <div className="flex flex-col h-full rounded-xl overflow-auto">
+      <div className="flex p-2 items-center justify-between border-b border-gray-300">
+        <div className="text-purple-600 p-2">Location</div>
+      </div>
+
+      <div className="p-2 flex flex-col gap-2 flex-1 bg-gray-100 ">
+        <input
+          className="text-base bg-gray-100"
+          value={search}
+          onChange={(evt) => setSearch(evt.target.value)}
+          placeholder="Search ..."
+        />
+
+        {!search && <div className="bg-purple-600 rounded-xl text-white p-2">{value.name}</div>}
+
+        <div className="flex flex-col gap-1 flex-1 overflow-auto" style={{ minHeight: 0 }}>
+          {predictions.map((prediction, index) => (
+            <div
+              key={index}
+              className="bg-gray-200 rounded-xl p-2 whitespace-nowrap overflow-hidden overflow-ellipsis"
+              onClick={() => onSelectPlace(prediction.place_id)}
+            >
+              {prediction.description}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
